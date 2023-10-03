104ca9dba7c721ef0649fdb48319328bf0fe404e
Leonardo <Leonardo@hotmail.com>
2023-10-03 17:14:36 +0200

    Cylinder volym metod fungerande. Cylinder volym knapp fungerande. skapade metod för alla setOnClickListeners.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -7,37 +7,76 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText x;
     EditText y;
-    Button phytagorasCalculate;
     TextView result;
+
+    Button phytagorasCalculate;
+    Button volumeCalculate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        phytagorasCalculate = findViewById(R.id.phytagoras_btn);
+
         x = findViewById(R.id.et_x);
         y = findViewById(R.id.et_y);
         result = findViewById(R.id.tv_result);
 
+        phytagorasCalculate = findViewById(R.id.phytagoras_btn);
+        volumeCalculate = findViewById(R.id.cylinder_btn);
+
+        setOnClick();
+
+    }
+
+
+
+    private void setOnClick() {
         phytagorasCalculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String xString = x.getText().toString();
-                float convertedX = Float.parseFloat(xString);
-                String yString = x.getText().toString();
-                float convertedY = Float.parseFloat(yString);
-                result.setText(String.valueOf(phytagoras(convertedX,convertedY)));
+                try {
+                    String xString = x.getText().toString();
+                    float convertedX = Float.parseFloat(xString);
+                    String yString = x.getText().toString();
+                    float convertedY = Float.parseFloat(yString);
+                    result.setText(String.valueOf(phytagoras(convertedX,convertedY)));
+                } catch(NumberFormatException s) {
+                    Toast.makeText(MainActivity.this, "Skriv in giltiga värden", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
+        volumeCalculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               try {
+                   String xString = x.getText().toString();
+                   double convertedX = Float.parseFloat(xString);
+                   String yString = x.getText().toString();
+                   double convertedY = Float.parseFloat(yString);
+                   result.setText(String.valueOf(volume(convertedX, convertedY)));
+               } catch (NumberFormatException s){
+                   Toast.makeText(MainActivity.this, "Skriv in giltiga värden", Toast.LENGTH_SHORT).show();
+               }
 
             }
         });
 
+
     }
 
+    private double volume (double r, double h){
+        r = r*r;
+
+        double volyme = Math.PI * r * h;
+        return volyme;
+
+    }
     private float phytagoras(float a, float b){
               a = a * a;
               b = b * b;
